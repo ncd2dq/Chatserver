a7c95d7ef1b09e77b3a9ed93b48de226b4d15d70
Nick Dima <ncd2dq@virginia.edu>
2017-10-29 15:52:06 -0500

    indentation problem with sublime, need to fix still

--- client.py
+++ client.py
@@ -2,56 +2,51 @@
 
 import socket
 import threading
+import time
 
 class Client(object):
-	def __init__(self, address=('217.0.0.1',5000)):
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.connect(address)
-		self.name = input('Please select a username: ')
+    def __init__(self, address=('127.0.0.1',5000)):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = input('Please select a username: ')
+        try:
+            self.s.connect(address)
+        except Exception as e:
+            print(e)
 
-	def encode(self, data):
-		return bytes(data, 'utf-8')
+    def encode(self, data):
+        return bytes(data, 'utf-8')
+    def decode(self, data):
+        return str(data, 'utf-8')
+    def _handle_recieving(self, buff=1024):
+        while True:
+            try:
+                data = self.s.recv(buff)
+                data = self.decode(data)
+                print(data)     
+    def _handle_sending(self):
+        while True:
+            try:
+                data = input('--> ')
+                data = self.name + ' ' + data
+                data = self.encode(data)
+                self.s.send(data)
+    def handler(self):
+        recieveThread = threading.Thread(target=self._handle_recieving)
+        recieveThread.daemon = True
+        recieveThread.start()
 
-	def decode(self, data):
-		return str(data, 'utf-8')
+        sendThread = threading.Thread(target=self._handle_sending)
+        sendThread.daemon = True
+        sendThread.start()
 
-	def _handle_recieving(self, buff=1024):
-		while True
-			try:
-				data = self.s.recv(buff)
-				data = self.decode(data)
-				print(data)
-
-			except Exception as e:
-				print(e)
-
-	def _handle_sending(self):
-		while True:
-			try:
-				data = input('--> ')
-				data = self.name + ' ' + data
-				data = self.encode(data)
-				self.s.send(data)
-
-			except Exception as e:
-				print(e)
-
-	def handler(self):
-		recieveThread = threading.Thread(target=self._handle_recieving)
-		recieveThread.daemon = True
-		recieveThread.start()
-
-		sendThread = threading.Thread(target=self._handle_sending)
-		sendingThread.daemon = True
-		sendingThread.start()
-
-	def run(self):
-		self.handler()
+    def run(self):
+        self.handler()
 
 
 def main():
-	client = Client()
-	client.run()
+    client = Client()
+    client.run()
 
 if __name__ == '__main__':
-	main()
+    main()
+    time.sleep(10)
--- server.py
+++ server.py
@@ -5,58 +5,58 @@ import threading
 
 
 class Server(object):
-	def __init__(self, address=('127.0.0.1',5000)):
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.bind(address)
-		self.s.listen(1)
-		self.connections = []
+    def __init__(self, address=('127.0.0.1',5000)):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(address)
+        self.s.listen(1)
+        self.connections = []
 
-	def encode(self, data):
-		return bytes(data, 'utf-8')
+    def encode(self, data):
+        return bytes(data, 'utf-8')
 
-	def _handle_connections(self):
-		'''
-		This method will create a chat thread for all socket-client connections
-		'''
-		print('Waiting for connections...')
-		while True:
-			conn, addr = self.s.accept()
-			self.connections.append(conn)
-			print('{} has connected'.format(str(addr)))
+    def _handle_connections(self):
+        '''
+        This method will create a chat thread for all socket-client connections
+        '''
+        print('Waiting for connections...')
+        while True:
+            conn, addr = self.s.accept()
+            self.connections.append(conn)
+            print('{} has connected'.format(str(addr)))
 
-			chatThread = threading.Thread(target=self._handle_chat, args=(conn,))
-			chatThread.daemon = True
-			chatThread.start()
+            chatThread = threading.Thread(target=self._handle_chat, args=(conn,addr))
+            chatThread.daemon = True
+            chatThread.start()
 
-	def _handle_chat(self, conn, addr, buff=1024):
-		'''
-		This method recieves data from a connection and sends it to all others
-		in self.connections. 
-		'''
-		while True:
-			try:
-				data = self.conn.recv(buff)
-				for connection in self.connections:
-					if connection != conn:
-						conn.send(data)
-			except:
-				self.disconnect(conn)
+    def _handle_chat(self, conn, addr, buff=1024):
+        '''
+        This method recieves data from a connection and sends it to all others
+        in self.connections. 
+        '''
+        while True:
+            try:
+                data = conn.recv(buff)
+                for connection in self.connections:
+                    if connection != conn:
+                        conn.send(data)
+            except:
+                self.disconnect(conn, addr)
 
-	def disconnect(self, conn):
-		'''This method removes closed sockets from the chat server list'''
-		self.connections.remove(conn)
-		msg = '{} has disconnected.'.format(str(addr))
-		msg = self.encode(msg)
-		for connection in self.connections:
-			connection.send(msg)
+    def disconnect(self, conn, addr):
+        '''This method removes closed sockets from the chat server list'''
+        self.connections.remove(conn)
+        msg = '{} has disconnected.'.format(str(addr))
+        msg = self.encode(msg)
+        for connection in self.connections:
+            connection.send(msg)
 
-	def run(self):
-		self._handle_connections()
+    def run(self):
+        self._handle_connections()
 
 
 def main():
-	server = Server()
-	server.run()
+    server = Server()
+    server.run()
 
 if __name__ == '__main__':
-	main()
+    main()
