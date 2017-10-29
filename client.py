cb26823ab0de9c949e4f79361cc792cb1fa100e8
Nick Dima <ncd2dq@virginia.edu>
2017-10-29 13:21:47 -0500

    built out Client methods, ready to test:

--- client.py
+++ client.py
@@ -7,22 +7,47 @@ class Client(object):
 	def __init__(self, address=('217.0.0.1',5000)):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.s.connect(address)
+		self.name = input('Please select a username: ')
 
 	def encode(self, data):
+		return bytes(data, 'utf-8')
 
 	def decode(self, data):
+		return str(data, 'utf-8')
 
-	def _handle_recieving(self):
+	def _handle_recieving(self, buff=1024):
+		while True
+			try:
+				data = self.s.recv(buff)
+				data = self.decode(data)
+				print(data)
+
+			except Exception as e:
+				print(e)
 
 	def _handle_sending(self):
+		while True:
+			try:
+				data = input('--> ')
+				data = self.name + ' ' + data
+				data = self.encode(data)
+				self.s.send(data)
+
+			except Exception as e:
+				print(e)
 
 	def handler(self):
-		#this should run the recieving / sending in 2 threads
-		#initiallize the threads here
+		recieveThread = threading.Thread(target=self._handle_recieving)
+		recieveThread.daemon = True
+		recieveThread.start()
+
+		sendThread = threading.Thread(target=self._handle_sending)
+		sendingThread.daemon = True
+		sendingThread.start()
 
 	def run(self):
 		self.handler()
-		
+
 
 def main():
 	client = Client()
