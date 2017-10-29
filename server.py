a6da39016e9e007278615d573e657970109fad92
Nick Dima <ncd2dq@virginia.edu>
2017-10-29 13:01:52 -0500

    added the disconnect method

--- server.py
+++ server.py
@@ -5,11 +5,14 @@ import threading
 
 
 class Server(object):
-	def __init__(self,address=('127.0.0.1',5000)):
+	def __init__(self, address=('127.0.0.1',5000)):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.s.bind(address)
 		self.s.listen(1)
 		self.connections = []
+
+	def encode(self, data):
+		return bytes(data, 'utf-8')
 
 	def _handle_connections(self):
 		'''
@@ -24,12 +27,25 @@ class Server(object):
 			chatThread.daemon = True
 			chatThread.start()
 
-	def _handle_chat(self, conn):
+	def _handle_chat(self, conn, addr, buff=1024):
 		'''
 		This method recieves data from a connection and sends it to all others
 		in self.connections. 
 		'''
+		while True:
+			try:
+				data = self.conn.recv(buff)
+				for connection in self.connections:
+					if connection != conn:
+						conn.send(data)
+			except:
+				self.disconnect(conn)
 
 	def disconnect(self, conn):
 		'''This method removes closed sockets from the chat server list'''
+		self.connections.remove(conn)
+		msg = '{} has disconnected.'.format(str(addr))
+		msg = self.encode(msg)
+		for connection in self.connections:
+			connection.send(msg)
 
